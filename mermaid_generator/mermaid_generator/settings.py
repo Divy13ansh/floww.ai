0ca866f29bdc09cebe6fed1881a9b2474b7801fd
Divy13ansh <chawladivyansh131106@gmail.com>
2025-06-14 02:34:36 +0530

    fixed minor bugs

--- mermaid_generator/mermaid_generator/settings.py
+++ mermaid_generator/mermaid_generator/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,7 +94,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
-<<<<<<< HEAD
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
@@ -101,8 +102,7 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
-=======
->>>>>>> 51b93bcb9b23d2529911f607383e507eb1bcf42b
+OPENROUTER_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
